063be4c83d3bb56de2076089fb36d928fe8f96ec
David Alsh <alshdavid@gmail.com>
2025-04-24 18:17:10 +1000

    npm

--- src/cmd/install_npm.rs
+++ src/cmd/install_npm.rs
@@ -1,8 +1,8 @@
 use std::fs;
 use std::time::SystemTime;
 
+use flate2::read::GzDecoder;
 use tar::Archive;
-use xz::read::XzDecoder;
 
 use super::install::InstallCommand;
 use crate::config::Config;
@@ -15,42 +15,31 @@ pub async fn install_from_npm(
   cmd: InstallCommand,
 ) -> anyhow::Result<()> {
   let start_time = SystemTime::now();
+
   let Some(version) = cmd.version else {
-    return Err(anyhow::anyhow!("Version not specified"));
+    panic!();
   };
+
   let version_safe = name::encode(&version)?;
-  let specifier = version;
-
   let target_temp = TempDir::new(&config.apvm_dir_temp.join(format!("{version_safe}.temp")));
-
-  let target = config.apvm_installs_dir.join("super").join(&version_safe);
-
-  if target.exists() && (cmd.force || specifier == "main") {
-    println!("Removing existing");
-    fs::remove_dir_all(&target)?;
-  } else if !cmd.force && target.exists() {
-    println!("✅ Already installed");
-    return Ok(());
-  }
+  let target = config.apvm_installs_dir.join("npm").join(&version_safe);
 
   let url = format!(
     "https://github.com/alshdavid-forks/atlaspack/releases/download/{}/{}",
-    &specifier,
-    c::TARBALL,
+    version,
+    c::TARBALL
   );
+  let response = reqwest::get(&url).await?;
 
-  println!("Downloading: {url}");
-
-  let response = reqwest::get(url).await?;
-
-  if response.status() == 404 {
-    return Err(anyhow::anyhow!("Version '{}' not found", &specifier));
+  if response.status() != 200 {
+    return Err(anyhow::anyhow!("Unable to fetch version"));
   }
 
+  println!("Downloading");
   let bytes = response.bytes().await?.to_vec();
 
   println!("Extracting");
-  let tar = XzDecoder::new(bytes.as_slice());
+  let tar = GzDecoder::new(bytes.as_slice());
   let mut archive = Archive::new(tar);
 
   archive.unpack(&target_temp)?;
@@ -64,7 +53,7 @@ pub async fn install_from_npm(
   println!(
     "✅ Installed in {:.2?} ({})",
     start_time.elapsed()?,
-    specifier
+    version
   );
 
   Ok(())
--- src/platform/constants.rs
+++ src/platform/constants.rs
@@ -1,17 +1,17 @@
 #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
-pub static TARBALL: &str = "atlaspack-darwin-arm64.tar.xz";
+pub static TARBALL: &str = "atlaspack-darwin-arm64.tar.gz";
 
 #[cfg(all(target_os = "macos", target_arch = "x86_64"))]
-pub static TARBALL: &str = "atlaspack-darwin-x64.tar.xz";
+pub static TARBALL: &str = "atlaspack-darwin-amd64.tar.gz";
 
 #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
-pub static TARBALL: &str = "atlaspack-linux-arm64.tar.xz";
+pub static TARBALL: &str = "atlaspack-linux-arm64.tar.gz";
 
 #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
-pub static TARBALL: &str = "atlaspack-linux-x64.tar.xz";
+pub static TARBALL: &str = "atlaspack-linux-amd64.tar.gz";
 
 #[cfg(all(target_os = "windows", target_arch = "aarch64"))]
-pub static TARBALL: &str = "atlaspack-windows-arm64.tar.xz";
+pub static TARBALL: &str = "atlaspack-windows-arm64.tar.gz";
 
 #[cfg(all(target_os = "windows", target_arch = "x86_64"))]
-pub static TARBALL: &str = "atlaspack-windows-x64.tar.xz";
+pub static TARBALL: &str = "atlaspack-windows-amd64.tar.gz";
